aacdfacdc9e06923bd2c6c8459e766e650eb210a
Vaibhav Patel <vp932712@gmail.com>
2017-12-14 20:31:55 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -57,7 +57,7 @@ def monopoly_with_fourth_tier
 monopoly[:"railroads"][:"names"][:"shortline"] ={}
 	monopoly[:"railroads"][:"names"][:"pennsylvania_railroad"] ={}
 	monopoly[:"railroads"][:"names"][:"b_and_o_railroad"] = {}
-	
+
 	monopoly[:"railroads"][:"names"][:"reading_railroad"][:"mortgage_value"] = "$100"
 	monopoly[:"railroads"][:"names"][:"pennsylvania_railroad"][:"mortgage_value"] = "$200"
 	monopoly[:"railroads"][:"names"][:"b_and_o_railroad"][:"mortgage_value"] = "$400"
