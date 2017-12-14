4c1a1732c996ac15d0fdc7991f43167b0eea9a56
Vaibhav Patel <vp932712@gmail.com>
2017-12-14 20:19:21 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,6 +20,7 @@ def monopoly_with_second_tier
 	monopoly[:"railroads"] = {}
 	monopoly[:"railroads"][:"pieces"] = 4
 	monopoly[:"railroads"][:"names"] = {}
+	monopoly[:"railroads"][:"rent_in_dollars"] = {}
 	monopoly
 end
 
