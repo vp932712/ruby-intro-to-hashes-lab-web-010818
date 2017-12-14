a1c746b538b838c1aee0082dd2f1bb9471bba3d5
Vaibhav Patel <vp932712@gmail.com>
2017-12-14 20:24:50 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,8 @@ def monopoly_with_third_tier
 	monopoly[:"railroads"][:"rent_in_dollars"][:"four_pieces_owned"] = 200
 	monopoly[:"railroads"][:"names"][:"reading_railroad"] ={}
 	monopoly[:"railroads"][:"names"][:"pennsylvania_railroad"] ={}
-	mono
+	monopoly[:"railroads"][:"names"][:"b_and_o_railroad"] = {}
+	
 	monopoly
 end
 
